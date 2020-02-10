e278304597ce87fd0f2de039b1b54941e9a9a037
Jeje01 <33060748+Jeje01@users.noreply.github.com>
2020-02-10 16:55:23 +0900

    Added done list
    Added finished list and set it interactive with to do list.

--- todo.js
+++ todo.js
@@ -1,10 +1,13 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
     toDoInput = toDoForm.querySelector("input"),
-    toDoList = document.querySelector(".js-toDoList");
+    toDoList = document.querySelector(".js-toDoList"),
+    doneList = document.querySelector(".js-finishedList");
 
-const TODOS_LS = 'toDos';
+const TODOS_LS = 'toDos',
+    FIN_LS = "done";
 
-let toDos = [];
+let toDos = []
+    done = [];
 
 function deleteToDo(event){
     const btn = event.target;
@@ -18,24 +21,108 @@ function deleteToDo(event){
     saveTodos();
 }
 
-function saveTodos(){
+function deleteFin(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    doneList.removeChild(li);
+    const cleanToDos = done.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    done = cleanToDos;
+    console.log(done);
+    saveDone();
+}
+
+function checkToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    paintCheck(li.querySelector("span").textContent);
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    console.log(toDos);
+    saveToDos();
+}
+
+function saveDone(){
+    localStorage.setItem(FIN_LS, JSON.stringify(done));
+}
+
+function rewindToDo(event){
+   const btn = event.target;
+   const li = btn.parentNode;
+   paintToDo(li.querySelector("span").textContent);
+   doneList.removeChild(li);
+   const cleanToDos = done.filter(function(toDo){
+        return toDo.id !==parseInt(li.id);
+    }); 
+    done = cleanToDos;
+    console.log(done);
+    saveDone();
+}
+
+
+function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintCheck(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const rewindBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = done.length + 1;
+    delBtn.style.border= '0px';
+    delBtn.style.borderRadius = '15px';
+    delBtn.style.marginLeft ='6px';
+    delBtn.style.opacity = '0.8';
+    rewindBtn.style.border= '0px';
+    rewindBtn.style.borderRadius = '15px';
+    rewindBtn.style.marginLeft ='6px';
+    rewindBtn.style.width ='29px';
+    rewindBtn.style.opacity = '0.8';
+    rewindBtn.innerText = "🡸";
+    rewindBtn.addEventListener("click", rewindToDo);
+    delBtn.innerText ="❌";
+    delBtn.addEventListener("click", deleteFin);
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(rewindBtn);
+    li.appendChild(delBtn);
+    li.id = newId;
+    doneList.appendChild(li);
+    const doneObj = {
+        text: text,
+        id: newId
+    }
+    done.push(doneObj);
+    saveDone();
 }
 
 function paintToDo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    const checkBtn = document.createElement("button");
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.style.border= '0px';
     delBtn.style.borderRadius = '15px';
     delBtn.style.marginLeft ='6px';
     delBtn.style.opacity = '0.8';
+    checkBtn.style.border= '0px';
+    checkBtn.style.borderRadius = '15px';
+    checkBtn.style.marginLeft ='6px';
+    checkBtn.style.opacity = '0.8';
+    checkBtn.innerText = "✔️";
+    checkBtn.addEventListener("click", checkToDo);
     li.style.marginBottom ='10px';
-    delBtn.innerText = "✔️";
+    delBtn.innerText = "❌";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
     li.appendChild(span);
+    li.appendChild(checkBtn);
     li.appendChild(delBtn);
     li.id = newId;
     toDoList.appendChild(li);
